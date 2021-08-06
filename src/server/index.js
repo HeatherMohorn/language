03f033b3c2f25f19c2fbe9de01848b64d21f0977
Heather Mohorn <heather.mohorn@gmail.com>
2021-08-06 11:24:29 -0400

    Broken though finished all steps

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,27 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-app.use(express.static('dist'))
+app.use(express.static('dist
+
+console.log(JSON.stringify(mockAPIResponse))
 
 console.log(__dirname)
 
@@ -12,9 +29,10 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
+
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
