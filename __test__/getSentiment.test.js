40eaefd4ed7ab46a7995032d968999d823ffafca
Heather Mohorn <heather.mohorn@gmail.com>
2021-08-25 11:46:00 -0400

    Suggsted changes

--- __test__/getSentiment.test.js
+++
@@ -1,9 +0,0 @@
-import { getSentiment } from "../src/client/js/formHandler"
-
-
-describe("Testing the getSentiment functionality", () => {
-
-    test("Testing the getSentiment() function", () => {
-
-           expect(getSentiment).toBeDefined();
-})});
