57c90d091fd2a380c8807935fce57c42bdf0572f
Heather Mohorn <heather.mohorn@gmail.com>
2021-08-26 16:00:48 -0400

    Everything is broken now

--- src/server/index.js
+++ src/server/index.js
@@ -2,43 +2,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 const { allowedNodeEnvironmentFlags } = require('process');
 
-var path = require('path')
-const express = require('express')
-var bodyParser = require('body-parser')
-const cors = require('cors')
-const fetch = require('node-fetch')
-
-var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
+var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
 
 const API_KEY = process.env.API_KEY;
 const baseURL = "https://api.meaningcloud.com/sentiment-2.1?key=";
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: false
 }))
 
-
-const getSentiment = async(userURL) => {
-    const res = await fetch(baseURL + API_KEY + "&lang=en&url=" + userURL);
-    try {
-        const data = await res.json();
-        console.log('data: ', data);
-        return data;
-    } catch(error) {
-        console.log('error', error);
-    }
-}
-
-
 app.use(express.static('dist'));
-
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -50,8 +31,10 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-
-
-app.post('http://localhost:8080/add', async function(req, res) {
-    res.send(await getSentiment(req.body.userURL));
-});
+app.post('/usertext', async(req, res) => {
+    const userURL = req.body.formText;
+    const response = await fetch(baseURL + API_KEY + "&lang=en&url=" + userURL)
+    .then(response => response.json())
+    .then(response => res.send(response))
+    .catch(error => console.log('error', error));
+  })
